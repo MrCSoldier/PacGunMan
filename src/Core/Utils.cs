caed933d52ee3d8745dc318b257b0f6976351129
Nullify <nullydev@protonmail.com>
2022-05-17 02:11:47 +0100

    cleared all warnings from source code

--- src/Core/Utils.cs
+++ src/Core/Utils.cs
@@ -8,16 +8,16 @@ namespace PacGunMan
             return Map.GetMap("test").Item1[x, y].ToString();
         }
 
-        public static void createNME()
-        {
-            Enemy e = new Enemy();
-            Random random = new Random();
-            e.ThreadWorker((int)random.NextInt64());
-        }
+        // public static void createNME()
+        // {
+        //     // Enemy e = new Enemy();
+        //     Random random = new Random();
+        //     e.ThreadWorker((int)random.NextInt64());
+        // }
 
-        public static void killNME(Enemy enemy, int ID)
-        {
-            enemy.kill(ID);
-        }
+        // public static void killNME(Enemy enemy, int ID)
+        // {
+        //     enemy.kill(ID);
+        // }
     }
 }
--- src/Entities/Enemy.cs
+++ src/Entities/Enemy.cs
@@ -1,40 +1,40 @@
-namespace PacGunMan.Entities
-{
-    public class Enemy
-    {
-        int ID;
-        int[] pos = new int[2];
-        int[] spawnPnt = new int[2];
-        Thread thread;
+// namespace PacGunMan.Entities
+// {
+//     public class Enemy
+//     {
+//         int ID;
+//         int[] pos = new int[2];
+//         int[] spawnPnt = new int[2];
+//         Thread thread;
 
-        // Dictionary<>
+//         // Dictionary<>
 
 
-        Dictionary<int, bool> liveThreads = new Dictionary<int, bool>() { };
-        Dictionary<int, int[]> threadLocation = new Dictionary<int, int[]>() { };
+//         Dictionary<int, bool> liveThreads = new Dictionary<int, bool>() { };
+//         Dictionary<int, int[]> threadLocation = new Dictionary<int, int[]>() { };
 
-        public void ThreadWorker(int ID)
-        {
-            this.ID = ID;
-            // this.pos = pos;
-            thread = new Thread(new ThreadStart(pulse));
-            liveThreads.Add(ID, true);
-            thread.Start();
-        }
+//         public void ThreadWorker(int ID)
+//         {
+//             this.ID = ID;
+//             // this.pos = pos;
+//             thread = new Thread(new ThreadStart(pulse));
+//             liveThreads.Add(ID, true);
+//             thread.Start();
+//         }
 
-        public void pulse()
-        {
-            while (liveThreads.TryGetValue(ID, out bool live))
-            {
-                Console.WriteLine(ID + ": Heart Beat");
-                Console.WriteLine($"{pos[0]}:{pos[1]}");
-                Thread.Sleep(1000);
-            }
-        }
+//         public void pulse()
+//         {
+//             while (liveThreads.TryGetValue(ID, out bool live))
+//             {
+//                 Console.WriteLine(ID + ": Heart Beat");
+//                 Console.WriteLine($"{pos[0]}:{pos[1]}");
+//                 Thread.Sleep(1000);
+//             }
+//         }
 
-        public void kill(int ID)
-        {
-            // thread.Abort(ID);
-        }
-    }
-}
+//         public void kill(int ID)
+//         {
+//             // thread.Abort(ID);
+//         }
+//     }
+// }
