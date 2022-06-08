9cd21fd5049318df796088c446e4a3b2ce7971ab
Nullify <46113492+MrCSoldier@users.noreply.github.com>
2022-06-08 10:32:38 +0100

    Added notations

--- src/Core/Collision.cs
+++ src/Core/Collision.cs
@@ -2,7 +2,7 @@ namespace PacGunMan
 {
     public static partial class Collision
     {
-
+        // create and initialize a Dictionary whre the keys are the objects and the values are the collision type.
         private static Dictionary<string, int> collidables = new() {
             { "#", 2 },
             { "│", 2 },
@@ -10,13 +10,19 @@ namespace PacGunMan
             { "X", 2 },
             { "=", 1 }
         };
-
+        
+        // static function that returns a boolean and takes x and y integers as well as a target collision type that is 2 by default 
         public static bool Check(int x, int y, int targetCollisionType = 2)
         {
+            /* if the target object in the coordinates exist within the Dictionary, 
+                    if the targetCollision type is less than or equal to colidables, return true. 
+                    Otherwise, return false 
+               Otherwise, return false. */
             if (collidables.ContainsKey(Utils.GetChar(x, y)))
             {
                 return targetCollisionType <= collidables[Utils.GetChar(x,y)] ? true : false;
             }
+            
             return false;
         }
     }
