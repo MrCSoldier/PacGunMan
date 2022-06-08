32ce26a3b25e5007081ebc760a414faf0714de64
Nullify <46113492+MrCSoldier@users.noreply.github.com>
2022-06-08 09:59:53 +0100

    Added Annotations.

--- src/Entities/Bullet.cs
+++ src/Entities/Bullet.cs
@@ -2,15 +2,20 @@ namespace PacGunMan.Entities
 {
     public class Bullet
     {
-
+        // function requires return of an integer variable. function requires an integer array and string variable
         public int[] Move(int[] xy, string direction) {
+            
             int[] newXY = new int[2];
             int[] chkXY = new int[2];
             chkXY = xy;
             
+            // check for any values listed in the switch cases, otherwise, return the current coordinates of entitiy.
             switch(direction) {
                 case "up":
+                    /* call the Collision Checking function by passing the target coordinates. 
+                    If true, return the same coordinate of entity (dont move). */
                     if (Collision.Check(chkXY[0] - 1, chkXY[1])) return new int[] { xy[0], xy[1] };
+                    // As the statement is false, the entity gets moved
                     return new int[] { xy[0] - 1, xy[1] };
                 case "down":
                     if (Collision.Check(chkXY[0] + 1, chkXY[1])) return new int[] { xy[0], xy[1] };
