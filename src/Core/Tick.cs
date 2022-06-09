6f778c870982c68322854d886cefb7c343e4c535
Nullify <46113492+MrCSoldier@users.noreply.github.com>
2022-06-09 15:29:34 +0100

    Update Tick.cs

--- src/Core/Tick.cs
+++ src/Core/Tick.cs
@@ -8,27 +8,36 @@ namespace PacGunMan
         static Entities.Manager.State eState = new();
         public static void Start()
         {
-            eState.List = eState.Add("@", new int[] { 2 * 3, 2* 5 });
+            /* assign a new value to the list by adding an element with the properties of:
+             Player Character: "@" and the position of x=6 and y=10 */
+            eState.List = eState.Add("@", new int[] { 6, 10 });
+            
+            //output what eState.GetInfo returns
             Console.WriteLine(eState.GetInfo());
-            // var player = 
+            
+            //initialize the direction of player's sight
             string direction = "right";
-            // Console.WriteLine("[Tick.Start]: " + entities.List.Count());
-            // entities.Add("@", new int[] { 3, 5 });
+            //initialize position array 
             int[] pos = Map.GetMap("test").Item2;
-            // Console.WriteLine("[Tick.Start]: " + eState.List.Count());
             var palyer = eState.List[0];
+            
+            //call the function by passing eState class 
             Update(eState, palyer, direction);
         }
 
         private static void Update(State eState, State player, string direction)
         {
+            /* set both eState as the value of Item1 and direction as the value of Item2 at the same time.
+            after callin it with the arguments of the eState class and the direction string */
             (eState, direction) = Input.Detect(eState, direction);
+            //call the Render function with the arguments of what GetMap returns from the Map 
             Graphics.Render(Map.GetMap("test").Item1, eState);
             Update(eState, player, direction);
         }
 
         private static string GetChar(int x, int y)
         {
+            //Get what the ascii-character at the coordinate is
             return Map.GetMap("test").Item1[x, y].ToString();
         }
     }
